cf91b69dd5ab72cf83c3d099e0d117b1c1f4baaa
Esteban Corrales <215191085+ecorralesv@users.noreply.github.com>
2026-02-04 10:37:34 -0600

    Cambios index, validaciones en js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,44 @@
 // Placeholder para futuras interacciones
 console.log("VT Solutions landing page cargada correctamente");
+
+//Seleccionar el formulario
+const form = document.getElementById("form-contacto");
+
+//Seleccionar los inputs
+const nombreInput = document.getElementById("nombre");
+const emailInput = document.getElementById("email");
+const mensajeInput = document.getElementById("mensaje");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); // evita recargar la página
+
+  const nombre = nombreInput.value.trim(); // quitar espacios
+  const email = emailInput.value.trim();
+  const mensaje = mensajeInput.value.trim();
+
+  // Validación del nombre
+  if(nombre.length < 3){
+      alert("El nombre debe ser mayor a 3 caracteres");
+      nombreInput.focus();
+      return;
+  }
+
+  // Validación del correo
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if(!emailRegex.test(email)){
+      alert("Ingrese un correo válido");
+      emailInput.focus();
+      return;
+  }
+
+  // Validación del mensaje
+  if(mensaje.length < 10){
+      alert("El mensaje debe tener al menos 10 caracteres");
+      mensajeInput.focus();
+      return;
+  }
+
+  // Si todo está bien
+  alert("¡Formulario enviado correctamente!");
+  form.reset(); // opcional: limpia el formulario
+});
